ad462463c47d2171c26edb5af32b839ede06870e
Anthony <alasam@live.com>
2022-01-15 10:26:00 -0500

    add product test

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
-end
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  RSpec.configure do |config|
+    config.use_transactional_fixtures = false
+  end
+
+    # SETUP
+    before :each do
+      @category = Category.create! name: 'Apparel'
+
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+
+    scenario "They see product detail" do
+      # ACT
+      visit root_path
+      find_link("Details »", match: :first).click
+
+      # DEBUG
+      # commented out b/c it's for debugging only
+      save_screenshot
+  
+      # VERIFY
+    expect(page).to have_content 'Description'
+    end
+  end
