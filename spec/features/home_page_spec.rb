69e4d1a55cdb5f5f0e3364baa08a82918482dd62
Anthony <alasam@live.com>
2022-01-15 09:26:42 -0500

    Update to home_page_spec

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+  RSpec.configure do |config|
+    config.use_transactional_fixtures = false
+  end
 
   scenario "They see all products" do
     # ACT
@@ -8,6 +11,9 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
 
     # DEBUG / VERIFY
     save_screenshot
+
+    expect(page).to have_css 'article.product'
   end
 
+  
 end
